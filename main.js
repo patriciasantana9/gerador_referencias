c418ce22117c4a9d1d8be4b322404af09e777847
Patrícia Santana <patriciasan3@gmail.com>
2023-03-18 14:30:01 -0300

    teste 1 (sobrenome)

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //pegar 4 inputs e botão ENVIAR
 
 let autor = document.querySelector("#autor");
-let titulo = document.querySelector("#titulo");
+let title = document.querySelector("#titulo");
 let disponivel = document.querySelector("#disponivel");
 let acesso = document.querySelector("#acesso");
 let button_send = document.querySelector(".enviar_informacoes");
@@ -13,10 +13,11 @@ let resultado = document.querySelector("section.resultado");
 function de_site() {
     const fname = "";
     const lname = "";
+    const names = autor.value;
     //console.log(autor.value.slice(-2));
-    
+    lname = names(names.lenght-1);
     
 
-    resultado.innerHTML = `${lname}, ${fname}. ${titulo.value}. Disponível em: ${disponivel.value}. Acesso em: ${acesso.value}`;
+    resultado.innerHTML = `${lname}, ${fname}. ${title.value}. Disponível em: ${disponivel.value}. Acesso em: ${acesso.value}`;
 }
 button_send.addEventListener('click', de_site);
