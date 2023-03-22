81cdd78de7b4dabef1250e3e4f5aad823eb7b237
Patrícia Santana <patriciasan3@gmail.com>
2023-03-22 19:53:31 -0300

    formatar data

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
-//pegar 4 inputs e botão ENVIAR
-
 let autor = document.querySelector("#autor");
 let title = document.querySelector("#titulo");
 let disponivel = document.querySelector("#disponivel");
 let acesso = document.querySelector("#acesso");
+
 let button_send = document.querySelector(".enviar_informacoes");
 
 let resultado = document.querySelector("section.resultado");
 
-//criar função que organiza referencia de site
-
+//referencia de site
 function de_site() {
 
     let full_name = autor.value;
+    //substrings
     let names = full_name.split(" ");
-    //"Patrícia", "Souza", "de", "Santana" //agora é array?
 
     let fname = " ";
-    for (let i = 0; i < names.length; i++) {
-        if (names[i] !== names[-1]) { //deve parar quando encontrar a última substring e manter um espaço entre as su.
-            fname += names[i] + " ";
-        }
+    for (let i = 0; i < names.length-1; i++) {
+        fname += names[i] + " ";    //eliminar espaço depois da penúltima substring
     }
     
     let lname = names[names.length-1];
+    //formatar data: DD mes AAAA
+    //let acesso_em = ;
 
-    resultado.innerHTML = `${lname}, ${fname}. ${title.value}. Disponível em: ${disponivel.value}. Acesso em: ${acesso.value}`;
+    resultado.innerHTML = `<p>${lname}, ${fname}. <strong>${title.value}</strong>. Disponível em: ${disponivel.value}. Acesso em: ${acesso_em} </p>`;
+
+    //https://willianjusten.com.br/formatando-datas-com-js-puro
+    //https://www.blogson.com.br/como-formatar-data-em-javascript-do-jeito-facil/
 }
 button_send.addEventListener('click', de_site);
+
+//quando faltar o nome do autor 
